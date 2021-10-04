2a7091627238b5aa1e0f17a0ef3876d7202485b0
Bryan Francois <b.francois.gj@gmail>
2021-10-04 11:27:23 +0200

    IMC fonctionnel via console.log

---
+++ Calculateurimc.js
@@ -0,0 +1,9 @@
+function validez () {
+    console.log ('validez')
+let poids = document.getElementById('poids').value
+let taille = document.getElementById('taille').value
+let imc= poids/(taille*taille)
+console.log (imc)
+//let valide = document.getElementById('valide')
+} 
+
