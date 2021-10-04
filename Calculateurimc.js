a5c2ad692ead46566f29c46038333b31118da87f
Bryan Francois <b.francois.gj@gmail>
2021-10-04 13:56:26 +0200

    Final

--- Calculateurimc.js
+++ Calculateurimc.js
@@ -5,3 +5,5 @@ let imc= poids/(taille*taille)
 document.getElementById ('resultats').value = imc
 } 
 
+// let poids = document.getElementById('poids').value // je recupere la valeur sur la balise html qui as pour id poids // .value // je recupere la valeur de l'input
+//document.getElementById ('resultats').value = imc // j'utilise la valeur de la balise html qui as pour id 'resultats' // = imc// Du coup mon resultats se fera dans cette case 
